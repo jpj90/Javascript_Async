a2778b674a45f5b1b7b32bf8d46a9d62b1fbb91c
James Janse <jpauljanse@gmail.com>
2018-11-17 22:51:52 +0100

    creating a custom extension method

--- 05_arrayConcatAll.js
+++ 05_arrayConcatAll.js
@@ -11,10 +11,35 @@ var exchanges = [
     ]
 ];
 
-console.log("stuff");
+// this is actually writing your own extension methode, using .prototype
+Array.prototype.concatAll = function() {
+    var results = [];
 
-exchanges.forEach(function(exchange){
-    exchange.forEach(function(stock){
-        console.log(JSON.stringify(stock));
-    })
+    /* this function will "flatten" the array, meaning that all 
+       previously nested items will be collected into one 
+       array, or in other words concatinated. do note however that
+       this function will only flatten an array by 1 dimension!
+       it will also not work on a flat array, because it assumes
+       there is a second dimension  */
+    this.forEach(function(subArray){
+        subArray.forEach(function(item){
+            results.push(item);
+        })
+    });
+
+    return results;
+}
+
+// THIS IS THE OLD FUNCTION
+// exchanges.forEach(function(exchange){
+//     exchange.forEach(function(stock){
+//         console.log(JSON.stringify(stock));
+//     })
+// });
+
+// THIS IS THE NEW FUNCTION, USING CONCATALL()
+var stocks = exchanges.concatAll();
+
+stocks.forEach(function(stock){
+    console.log(JSON.stringify(stock));
 });
