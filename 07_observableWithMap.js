9400208e6ae55fc3c3f766dae994abf3f446e005
James Janse <jpauljanse@gmail.com>
2018-11-18 17:52:51 +0100

    using map function differently seems to help, removed Observable.interval

--- 07_observableWithMap.js
+++ 07_observableWithMap.js
@@ -1,19 +1,13 @@
 // https://egghead.io/lessons/javascript-using-the-map-method-with-observable
 
-var {range, map } = require('rxjs');
-const Observable = require('rxjs');
+const { Observable, Subject, ReplaySubject, from, of, range } = require('rxjs');
+const { map, filter, switchMap } = require('rxjs/operators');
 
-range(5,4).forEach(item => console.log(item));
+/* CommonJS example from https://github.com/ReactiveX/rxjs
+range(1, 200)
+  .pipe(filter(x => x % 2 === 1), map(x => x * 2))
+  .subscribe(x => console.log(x));
+*/
+  range(1,10)
+    .pipe(map(function(item){ return item * 5 })).forEach(out => console.log(out));
 
-[1,2,3].map(function(item){
-    return item * 100;
-}).forEach(function(thing){
-    console.log(thing);
-});
-
-
-var observable = Observable.interval(1000);
-
-observable.forEach(function(item){
-    console.log(item);
-})
