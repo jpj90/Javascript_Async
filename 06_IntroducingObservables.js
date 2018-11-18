ecee18c0db69616a7caf103b3c29ef1fa1a4087f
James Janse <jpauljanse@gmail.com>
2018-11-18 11:18:26 +0100

    got stuff working for import RxJS

--- 06_IntroducingObservables.js
+++ 06_IntroducingObservables.js
@@ -1,13 +1,14 @@
 // https://egghead.io/lessons/rxjs-introducing-the-observable
-import {  Rx } from 'rxjs';
+var Observable = require('rxjs')
+
 
 // an Observable can be seen as 'a type to model an asynchronous
 // operation as a collection. to be be precise, it can be seen
 // as a collection that arrives over time. it can also be 
 // thought of as an "event stream".'
 
-var Observable = Rx.Observable;
+var observable = Observable.interval(1000);
 
-[1,2,3].forEach(function(i){
+observable.forEach(function(i){
     console.log(i);
 })
